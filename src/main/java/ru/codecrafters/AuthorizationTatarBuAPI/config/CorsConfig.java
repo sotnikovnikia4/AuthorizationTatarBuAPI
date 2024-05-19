69ccf253d7536703d2687c6e5f9fd98eb6bea1ce
sotnikovnikia4 <sotnikovnikita4@gmail.com>
2024-05-19 07:54:43 +0300

    final

--- src/main/java/ru/codecrafters/AuthorizationTatarBuAPI/config/CorsConfig.java
+++ src/main/java/ru/codecrafters/AuthorizationTatarBuAPI/config/CorsConfig.java
@@ -1,24 +1,20 @@
 package ru.codecrafters.AuthorizationTatarBuAPI.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Random;
-
 @Configuration
-public class UsefulBeans {
+public class CorsConfig {
 
     @Bean
-    public ModelMapper modelMapper(){
-        return new ModelMapper();
-    }
-
-    @Bean
-    public Random random(){
-        return new Random();
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 }
